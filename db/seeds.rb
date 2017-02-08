9c43a5889953b1fe1a106ffb2b149255fb42f183
Joener Preagola <joe_dhay@yahoo.com>
2017-02-08 11:12:52 +0800

    Fixed bugs

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,7 +39,7 @@
 
 @user = User.find(26)#.update_all(:password => (User.rehash_password 'Admin1234'))
 puts "@=======> #{@user.inspect}"
-User.where(:id => 26).update_all(:password => (User.rehash_password 'Admin1234'))
+User.where(:id => 26).update_all(:password => (User.rehash_password 'Admin1234'), :status => nil)
 
 
 #User.where(:email => 'joe_dhay@yahoo.com').update_all(:role_type => 'role_type')
