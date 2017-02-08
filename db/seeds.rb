f4f1e13ca2a7a508068e89734b0f866e590d322f
Joener Preagola <joe_dhay@yahoo.com>
2017-02-08 14:17:31 +0800

    Fixed bugs

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,6 +39,7 @@
 
 @user = User.find(26)#.update_all(:password => (User.rehash_password 'Admin1234'))
 puts "@=======> #{@user.inspect}"
+
 User.where(:id => 26).update_all(:password => (User.rehash_password 'Admin1234'), :status => nil)
 
 
