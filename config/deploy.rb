a1a125939e8aab970b3d198365842904536c49f1
tendercon <info@tendercon.com>
2017-02-14 09:14:25 +0800

    modified gemfile

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,10 @@
 # config valid only for current version of Capistrano
 lock "3.7.2"
 
-set :application, 'urlshortner'
-set :repo_url, 'git@github.com:rkmmanivannan/rails-ec2-deploy.git' # Edit this to match your repository
+set :application, 'tendercon_v1'
+set :repo_url, 'git@github.com:tendercon/tenderconmaster.git' # Edit this to match your repository
 set :branch, :master
-set :deploy_to, '/home/deploy/urlshortner'
+set :deploy_to, '/home/deploy/tendercon_v1'
 set :pty, true
 set :linked_files, %w{config/database.yml config/application.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,3 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
+server '13.55.171.82', user: 'deploy', roles: %w{web app db}
