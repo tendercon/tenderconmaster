7dde27d197c9f86482cd8026e49409ac1257d3b8
tendercon <info@tendercon.com>
2017-02-16 08:46:46 +0800

    modified gemfile

--- config/deploy.rb
+++ config/deploy.rb
@@ -86,11 +86,11 @@ namespace :deploy do
     on roles(delayed_job_roles) do |host|
       within release_path do
         # Only install if not already present
-        unless test("[ -f #{release_path}/#{delayed_job_bin} ]")
+        #unless test("[ -f #{release_path}/#{delayed_job_bin} ]")
           with rails_env: fetch(:rails_env) do
             execute :bundle, :exec, :rails, :generate, :delayed_job
           end
-        end
+        #end
       end
     end
   end
