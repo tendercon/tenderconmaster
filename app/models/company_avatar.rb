e0b2c6038773ad6475b32424d7939cfa617359af
tendercon <info@tendercon.com>
2017-02-24 16:16:24 +0800

    modified quotes and rfi

--- app/models/company_avatar.rb
+++ app/models/company_avatar.rb
@@ -7,9 +7,7 @@ class CompanyAvatar < ActiveRecord::Base
   #                   :path => ":rails_root/public/assets/company_avatar/image/:id/:style/:basename.:extension"
   # validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
   # validates_attachment_size :image, :in => 0.megabytes..1.megabytes
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png",
-  :path  => ":rails_root/public/system/:class/:attachment/:style/:filename",
-      :url => "/system/:class/:attachment/:style/:filename"
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
   #has_attached_file :image,:style => {:large => '300x300<',:medium => '300x300>',:thumb => '100x100'},
   #                  :url  =>  ":rails_root/public/assets/avatar/image/:id/:style/:basename.:extension",
