42cc2b18a776d23c1c062979476e182450eaa504
tendercon <info@tendercon.com>
2017-03-17 20:51:24 +0800

    add staging

--- config/deploy.rb
+++ config/deploy.rb
@@ -78,7 +78,7 @@ namespace :deploy do
     on primary :db do
       within current_path do
         with rails_env: fetch(:rails_env) do
-          execute :rake, 'db:seed'
+          execute :bundle, :exec, :rake, 'db:seed'
         end
       end
     end
@@ -89,7 +89,7 @@ namespace :deploy do
     on primary :db do
       within current_path do
         with rails_env: fetch(:rails_env) do
-          execute :rake, 'db:migrate'
+          execute :bundle, :exec, :rake, 'db:migrate'
         end
       end
     end
