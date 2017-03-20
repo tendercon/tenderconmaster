964e67c2732d0aeb12132a55917a8fcd94e6a580
tendercon <info@tendercon.com>
2017-03-20 22:02:07 +0800

    add cms

--- app/controllers/landing_page_controller.rb
+++ app/controllers/landing_page_controller.rb
@@ -2,7 +2,7 @@ class LandingPageController < ApplicationController
   before_filter :is_logged?, except: [:index]
   skip_before_action :verify_authenticity_token
 
-  layout 'test_layout', :on => [:index]
+  layout 'landing_layout', :on => [:index]
   def index
 
   end
