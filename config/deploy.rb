57159ac8d477c12959d04f140b87e50b1a243e4b
tendercon <info@tendercon.com>
2017-03-21 15:23:22 +0800

    modified login1

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock "3.7.2"
 set :application, 'tendercon_v1'
 set :repo_url, 'git@github.com:tendercon/tenderconmaster.git' # Edit this to match your repository
 set :branch, :master
-set :deploy_to, '/home/deploy/tendercon_v1'
+set :deploy_to, '/home/deploy/tendercon_v1/'
 set :pty, true
 set :linked_files, %w{config/database.yml config/application.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
