83ddfb2994beb6a76c1e8a2e75f10672231188bc
tendercon <info@tendercon.com>
2017-03-21 15:19:42 +0800

    modified login1

--- config/deploy.rb
+++ config/deploy.rb
@@ -55,7 +55,7 @@ set :puma_preload_app, false
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
-#after 'deploy:publishing', 'deploy:migrate'
+after 'deploy:publishing', 'deploy:migrate'
 namespace :deploy do
   def delayed_job_roles
     fetch(:delayed_job_server_role, :app)
